54c0a49b4946c48cc506e1fb1874cbb224477354
wnma3mz <wnma3mz@163.com>
2018-08-07 12:29:56 +0800

    fix animation var line bug

--- GA_Visual/GA_main.py
+++ GA_Visual/GA_main.py
@@ -2,7 +2,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.animation as animation
-
+# GA——遗传算法，SA模拟退火算法
 # 每个个体序列长度
 DNA_SIZE = 10
 # 种群个体数目
@@ -19,6 +19,7 @@ X_BOUND = [0, 5]
 
 def F(x):
     # 目的：找到下面这个函数的最值
+    # sin(10x)*x+cos(2x)*x
     return np.sin(10 * x) * x + np.cos(2 * x) * x
 
 
@@ -28,6 +29,7 @@ def get_fitness(pred):
     return pred - np.min(pred) + 1e-3
     # 找到函数中的最小值
     # return np.max(pred) - pred + 1e-3
+
 
 def translateDNA(pop):
     # 将个体中DNA转换为所需的数据（二进制换为十进制），并且归一化到指定范围（这里是[0，5]）
@@ -69,37 +71,22 @@ def mutate(child):
 
 # 以上都是遗传算法的主要代码，下面是动态可视化的代码
 
-def simPoints(simData):
-    x, y = simData[0], simData[1]
-    line.set_data(x, y)
-    return line
-
 
 def simData():
-    # 第一代种群个体
     pop = np.random.randint(1, size=(POP_SIZE, DNA_SIZE))
     for _ in range(N_GENERATIONS):
-        # 将种群每个个体放入函数中
         F_values = F(translateDNA(pop))
 
-        # 这里选取每代种群中出现频率最大的个体
-        vals, counts = np.unique(translateDNA(pop), return_counts=True)
-        max_x = vals[np.argmax(counts)]
-        print(max_x)
+        i = np.argmax(F_values)
+        max_x = translateDNA(pop)[i]
 
-        # 进行比较之后的值
         fitness_score = get_fitness(F_values)
-        # 筛选，选出优质下一代
         pop = select(pop, fitness_score)
         pop_copy = pop.copy()
 
-        # 每个个体进行交配
         for parent in pop:
-            # 将当前选中的个体与种群中其他个体进行交配
             child = crosscover(parent, pop_copy)
-            # 交配完的个体可能产生变异
             child = mutate(child)
-            # 新个体取代原来的父/母
             parent[:] = child
         # 将需要可视化的点x，y值传递给另一个函数
         yield max_x, F(max_x)
@@ -124,8 +111,8 @@ def convert_gif():
         # 将种群每个个体放入函数中
         F_values = F(translateDNA(pop))
 
-        vals, counts = np.unique(translateDNA(pop), return_counts=True)
-        max_x = vals[np.argmax(counts)]
+        i = np.argmax(F_values)
+        max_x = translateDNA(pop)[i]
 
         print(max_x)
         plt.plot(max_x, F(max_x), ".", color='blue')
@@ -145,28 +132,35 @@ def convert_gif():
             child = mutate(child)
             # 新个体取代原来的父/母
             parent[:] = child
-        # plt.pause(0.1)
+        plt.pause(0.1)
         # 首先保存每次迭代的图片
-        plt.savefig("{}.png".format(_), format='png')
+        # plt.savefig("{}.png".format(_), format='png')
         # 将图片用这个函数进行读取（转换为array）
-        frames.append(imageio.imread("{}.png".format(_)))
+        # frames.append(imageio.imread("{}.png".format(_)))
     # 最后使用这个函数将所有的图片顺序合成一个gif图片，duration是间隔时间，单位是s
-    imageio.mimsave("imageio.gif", frames, 'GIF', duration=0.2)
+    # imageio.mimsave("imageio.gif", frames, 'GIF', duration=0.2)
 
     plt.ioff()
     plt.show()
 
 
 def convert_mp4():
+
     # 绘制底板
     fig = plt.figure()
     ax = fig.add_subplot(111)
+
     x = np.linspace(*X_BOUND, 200)
     # 绘制函数图像
     plt.plot(x, F(x))
 
     # 绘制动态展示的控件——点
     line, = ax.plot([], [], 'bo', ms=10)
+
+    def simPoints(simData):
+        x, y = simData[0], simData[1]
+        line.set_data(x, y)
+        return line
     # 使用这个函数，fig是本身的画板，simPoints是需要绘制的点，simData是产生数据的函数传递给simPoints，interval动画间隔时间，单位是ms
     ani = animation.FuncAnimation(
         fig, simPoints, simData, interval=200)
@@ -174,7 +168,7 @@ def convert_mp4():
     # 查看目前环境支持保存的格式。import matplotlib.animation as animation; animation.writers.list()
 
     # 一般不下载第三方工具，保存格式只有html
-    ani.save('animation.htm', dpi=70)
+    # ani.save('animation.htm', dpi=70)
 
     # 使用命令conda install -c conda-forge ffmpeg进行下载安装
     # ani.save('animation.mp4', writer='ffmpeg')
@@ -184,5 +178,5 @@ def convert_mp4():
 
 if __name__ == '__main__':
     # 两种方式都可以转换为gif
-    convert_gif()
-    # convert_mp4()
+    # convert_gif()
+    convert_mp4()
