c8fc4a2f10c76b84e84611862c9a278fa3ba3a3e
wnma3mz <wnma3mz@163.com>
2018-08-08 12:27:48 +0800

    update SA 领域

--- SA_Compare_map&vectorized/SA.py
+++ SA_Compare_map&vectorized/SA.py
@@ -7,7 +7,7 @@ import matplotlib.animation as animation
 T = 100
 # 最低温度
 T_min = 1e-8
-# 迭代次数
+# 解个数
 k = 100
 # 温度下降率
 delta = 0.98
@@ -49,21 +49,21 @@ def move2(num, t):
 
 def getSA():
     # 初始化初始解
-    x = np.random.random(k)
+    x = np.random.random(k) * (X_BOUND[1] - X_BOUND[0])
     t = T
     # 开始退火
     while (t > T_min):
         # 对每个可能值进行移动
         for i, num in enumerate(x):
             Fnum = F(num)
-            # 向右移动的范围
-            x_new = num + (np.random.rand() * 2 - 1) * t
+            # 左右移动的范围，由自己决定
+            x_new = num + (np.random.rand() * 2 - 1)
             # 保证移动后的范围在区间内
             if (0 <= x_new and x_new <= 5):
                 # 移动后的值
                 Fnum_new = F(x_new)
                 # 寻找最小值。如果新值小于原来的值，那么就进行替换
-                if (Fnum_new > Fnum):
+                if (Fnum_new < Fnum):
                     # 寻找最大值。如果新值大于原来的值，那么就进行替换
                     # if (Fnum_new > Fnum):
                     x[i] = x_new
@@ -74,9 +74,9 @@ def getSA():
                         x[i] = x_new
         # 每次退火的比率
         t *= delta
-        print("x坐标：{}, y坐标：{}".format(x[np.argmin(F(x))], np.min(F(x))))
-        # yield x[np.argmin(F(x))], np.min(F(x))
-        yield x[np.argmax(F(x))], np.max(F(x))
+        # print("x坐标：{}, y坐标：{}".format(x[np.argmin(F(x))], np.min(F(x))))
+        yield x[np.argmin(F(x))], np.min(F(x))
+    # return x[np.argmax(F(x))], np.max(F(x))
 
 
 def test_map():
@@ -111,7 +111,7 @@ if __name__ == '__main__':
     plt.plot(x, F(x))
     line, = ax.plot([], [], 'bo', ms=10)
     ani = animation.FuncAnimation(
-        fig, simPoints, getSA, interval=200)
+        fig, simPoints, getSA, interval=400)
 
     # x = test_vectorized()
     # print("x坐标：{}, y坐标：{}".format(x[np.argmin(F(x))], np.min(F(x))))
