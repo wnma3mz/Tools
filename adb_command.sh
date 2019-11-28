c12402104b78cce50a06e36de1a51c8990ccfabf
wnma <wnma3mz@163.com>
2019-11-28 17:57:47 +0800

    Create adb_command.sh

---
+++ adb_command.sh
@@ -0,0 +1,38 @@
+# 启动app
+adb shell monkey -p package -c android.intent.category.LAUNCHER 1
+
+# 强行关闭app
+adb shell am force-stop package
+
+# 查看当前运行的app
+adb shell dumpsys window | grep mCurrentFocus
+
+# 列出所有app
+adb shell pm list package
+
+# 手机屏幕状态
+adb shell dumpsys window policy
+
+# 拍照
+adb shell input keyevent 27
+# 电源键
+adb shell input keyevent 26
+# 返回健
+adb shell input keyevent 4
+# 传输手机文件到本地
+adb pull /sdcard//DICM1.png .
+# 截图
+adb shell screencap -p /sdcard/xx.png
+"""
+# 使用多线程加快点击某点的速度
+def run_adb_shell(num, index):
+    for _ in range(num):
+        os.system("adb shell tap 1016, 444")
+
+def tap_screen():
+    import threading
+    for i in range(10):
+        threading.Thread(target=run_adb_shell, args=(20000, i)).start()
+        time.sleep(1)
+
+"""
